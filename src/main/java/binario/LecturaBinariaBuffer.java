4c4e106b7f0cce3e2bd4afddd8d6bb1ab79f590b
Josemedvel <josemedvel@gmail.com>
2025-09-10 09:14:51 +0200

    primer commit

---
+++ src/main/java/binario/LecturaBinariaBuffer.java
@@ -0,0 +1,22 @@
+package binario;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.util.ArrayList;
+
+public class LecturaBinariaBuffer {
+    public static void main(String[] args){
+        try {
+            DataInputStream ds = new DataInputStream(new BufferedInputStream(new FileInputStream("salida_escritura_binaria.txt")));
+            ArrayList<Integer> array = new ArrayList<>(1000);
+            for (int i = 0; i < 1000; i++) {
+                array.add(i, ds.readInt());
+            }
+            ds.close();
+            System.out.println(array);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
